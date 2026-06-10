801d2666b3d6005806d15628785e5b6335810460
MateoRc <mateorestrepociro13@gmail.com>
2026-06-10 18:19:54 -0500

    Second commit

--- ReservasWEB2-20260610T012004Z-3-001/ReservasWEB2/ClientApp/src/app/components/acceso-denegado/acceso-denegado.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-acceso-denegado',
-  templateUrl: './acceso-denegado.component.html'
-})
-export class AccesoDenegadoComponent {
-}
